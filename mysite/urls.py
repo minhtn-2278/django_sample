94a0c68274e07dfe34f10f6cd795f6829c9a5428
minhtn-2278 <tran.nhat.minh-b@sun-asterisk.com>
2023-01-16 10:49:46 +0700

    Polls app

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_sample project.
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/dev/topics/http/urls/
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
