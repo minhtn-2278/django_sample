e0bc9273bc81ca04cb4a339492399497ead71085
minhtn-2278 <tran.nhat.minh-b@sun-asterisk.com>
2023-02-08 10:16:29 +0700

    Todo API

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from todo_api import urls as todo_urls
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('todos/', include(todo_urls)),
 ]
